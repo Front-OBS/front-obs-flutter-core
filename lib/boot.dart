41d5645445bd4c03d834ba4c4c29a18c8789814c
vsabenin <mvolkov.apps@gmail.com>
2023-10-30 15:56:40 +0300

    - трейсы

--- lib/boot.dart
+++ lib/boot.dart
@@ -28,7 +28,7 @@ QAToolsGuardMain<TEnv extends IApplicationEnvironment>(
 launchWithoutOberon<TEnv extends IApplicationEnvironment>(
     TEnv env, Launcher<TEnv> launcher) async {
   WidgetsFlutterBinding.ensureInitialized();
-  //await LogVault.initVault(false);
+  await LogVault.initVault(false);
   //initServices();
   launcher(
       env,
