dba2c3a3dcf96a27c40c508e1d803ac9d05f4f52
vsabenin <mvolkov.apps@gmail.com>
2023-11-21 12:14:25 +0300

    1.1.1

--- lib/environments.dart
+++ lib/environments.dart
@@ -1,9 +1,6 @@
 abstract class IApplicationEnvironment {
   abstract final String envTitle;
   abstract final String projectKey;
-
-  IApplicationEnvironment fromJson(Map<String, dynamic> json);
-  Map<String, dynamic> toJson();
 }
 
 class ApplicationOptions<TEnv extends IApplicationEnvironment> {
