061ad9ef90bc2d2f6effc485fed8113774f711f0
vsabenin <mvolkov.apps@gmail.com>
2023-11-23 18:06:34 +0300

    1.1.3

--- lib/oberon_connector.dart
+++ lib/oberon_connector.dart
@@ -3,3 +3,6 @@ library oberon_connector;
 export "boot.dart";
 export "log.dart";
 export 'observers.dart';
+export 'observers/navigation.dart' hide OberonNavigationObserver;
+export 'observers/network_log.dart' hide OberonDioInterceptor;
+export 'observers/ui.dart' hide ScrollDetector;
